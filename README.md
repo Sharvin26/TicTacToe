fdfe3493b2005adbe4c4351b5d6a1a1a48a0347d
Sharvin Shah <sharvinshah51@gmail.com>
2019-12-20 16:32:14 +0530

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# TicTacToe
+# TicTacToe Game
+
+This is a tictactoe game built using python3.7. It's a terminal based application.
+
+### Prerequistes:
+* Python 3.7 required.
+* No external pip packages are required..
+
+### To run the game use command:
+`python3.7 tictactoe.py`
+
+### Game Example:
+
+```
+➜ python3.7 tictactoe.py
+Enter the size of tictactoe game you wanna play? 2
+   0  1
+0 [0, 0]
+1 [0, 0]
+Current player: 1
+What column do you want to play? ( eg: 0, 1, 2 ): 0
+What row do you want to play? ( eg: 0, 1, 2 ): 1
+   0  1
+0 [0, 0]
+1 [1, 0]
+Current player: 2
+What column do you want to play? ( eg: 0, 1, 2 ): 0
+What row do you want to play? ( eg: 0, 1, 2 ): 1
+This position is occupied. Choose another
+What column do you want to play? ( eg: 0, 1, 2 ): 1
+What row do you want to play? ( eg: 0, 1, 2 ): 0
+   0  1
+0 [0, 2]
+1 [1, 0]
+Current player: 1
+What column do you want to play? ( eg: 0, 1, 2 ): 0
+What row do you want to play? ( eg: 0, 1, 2 ): 0
+   0  1
+0 [1, 2]
+1 [1, 0]
+Player 1 is the winner vertically ( | ) !!
+Game is over, would you like to play again?? ( y/n ): n
+GoodBye....
+```
--- tictactoe.py
+++ tictactoe.py
@@ -1,36 +1,49 @@
 import itertools
 
-def game_board(game_map, player = 0, row = 0, column = 0, just_display = False):
+def game_board(game_map, player = 0, row = 0, 
+               column = 0, just_display = False):
     try:
-
-        print("   0  1  2")
+        if game_map[row][column] != 0:
+            print("This position is occupied. Choose another")
+            return game_map, False
+        print("   " + "  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
             game_map[row][column] = player
         for count,row in enumerate(game_map):
             print(count, row)
-        return game_map
+        return game_map, True
     
     except IndexError as e:
-        print("Error: Make sure you input row/column between 0 and 2.", e)
+        print("Error: Make sure you input row/column available.", e)
+        return game_map, False
     
     except Exception as e:
         print("Something went very wrong", e)
+        return game_map, False
 
 def win(current_game):
 
+    def all_same(l):
+        if l.count(l[0]) == len(l) and l[0] != 0:
+            return True
+        else:
+            return False
+
     # Horizontal Winner.
     for row in current_game:
-        if row.count(row[0]) == len(row) and row[0] != 0:
+        if all_same(row):
             print(f"Player {row[0]} is the winner horizontally ( - ) !!")
+            return True
     
     # Vertical Winner.
     for col in range(len(current_game)):
         check = []
         for row in current_game:
             check.append(row[col])
-        if check.count(check[0]) == len(check) and check[0] != 0:
+        if all_same(check):
             print(f"Player {check[0]} is the winner vertically ( | ) !!")
-
+            return True
+    
     # Diagonal Winner.
     cols = reversed(range(len(current_game)))
     rows = range(len(current_game))
@@ -38,32 +51,50 @@ def win(current_game):
     diags = []
     for col,row in zip(cols, rows):
         diags.append(current_game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally ( / ) !!")
+        return True
 
     diags = []
     for ix in range(len(current_game)):
         diags.append(current_game[ix][ix])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally ( \\ )!!")
+        return True
+    
+    return False
+
 
 play = True
 players = [1, 2]
 
 while play:
-    game = [[ 0, 0, 0 ],
-            [ 0, 0, 0 ],
-            [ 0, 0, 0 ]]
 
+    game_size = int(input("Enter the size of tictactoe game you wanna play? "))
+    game = [ [ 0 for i in range(game_size) ] for i in range(game_size) ]
     game_won = False
-    game = game_board(game, just_display = True)
+    game, _ = game_board(game, just_display = True)
     player_choice = itertools.cycle([1,2])
     while not game_won:
         current_player = next(player_choice)
         print(f"Current player: {current_player}")
-        row_choice = int(input("What row do you want to "
-                            "play? ( 0, 1, 2 ): "))
-        column_choice = int(input("What column do "
-                               "you want to play? ( 0, 1, 2 ): "))
-        game = game_board(game, current_player, row_choice, 
-                          column_choice)
+        played = False
+        while not played:
+            column_choice = int(input("What column do "
+                                "you want to play? ( eg: 0, 1, 2 ): "))
+            row_choice = int(input("What row do you want to "
+                                "play? ( eg: 0, 1, 2 ): "))
+            game, played = game_board(game, current_player, row_choice, 
+                            column_choice)
+        if win(game):
+            game_won = True
+            again = input("Game is over, would you like"
+                          " to play again?? ( y/n ): ")
+            if again.lower() == 'y' or again.lower() == 'yes':
+                print("Restarting...")
+            elif again.lower() == 'n' or again.lower() == 'no':
+                print("GoodBye....")
+                play = False
+            else:
+                print("Not a valid ans so...see you later...")
+                play = False
